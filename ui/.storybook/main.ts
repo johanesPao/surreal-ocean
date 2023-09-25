382fa2794bf11f3b67903484ed14933fb8a926ea
Johanes Indra Pradana Pao <johanes.pao@gmail.com>
2023-09-25 11:26:07 +0700

    Penggunaan addon remarkGfm dan switch docs false pada @storybook/addon-essentials

--- ui/.storybook/main.ts
+++ ui/.storybook/main.ts
@@ -1,13 +1,29 @@
+import remarkGfm from "remark-gfm";
+
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
+    {
+      name: "@storybook/addon-essentials",
+      options: {
+        docs: false,
+      },
+    },
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
-    "@storybook/addon-mdx-gfm"
+    {
+      name: "@storybook/addon-docs",
+      options: {
+        mdxPluginOptions: {
+          mdxCompileOptions: {
+            remarkPlugins: [remarkGfm],
+          },
+        },
+      },
+    },
   ],
   framework: {
     name: "@storybook/react-vite",
