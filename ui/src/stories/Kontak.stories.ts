33fa90359dfa7d8edb991107b2f0bc2758796ce0
Johanes Indra Pradana Pao <johanes.pao@gmail.com>
2023-10-04 08:54:27 +0000

    Normal Story komponen Kontak

--- ui/src/stories/Kontak.stories.ts
+++ ui/src/stories/Kontak.stories.ts
@@ -1,17 +1,44 @@
 import { Meta, StoryObj } from "@storybook/react";
 
 import Kontak from "../komponen/Kontak";
+import { EWarna } from "../enum";
+
+const warnaNormalProps = {
+  warnaIkon: EWarna.PRIMER,
+  warnaTeks: EWarna.PRIMER
+}
 
 const meta = {
   title: "Surreal Ocean/Komponen/Kontak",
   component: Kontak,
   tags: ["autodocs"],
-  argTypes: {},
+  argTypes: {
+    data: { control: { type: "object" } },
+    bgKontak: { control: { type: "color", presetColor: EWarna.TEKS } }
+  },
 } satisfies Meta<typeof Kontak>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Normal: Story = {
-  args: {},
+  args: {
+    data: [
+      {
+        mode: "GMAIL",
+        teks: "johanes.pao",
+        ...warnaNormalProps
+      },
+      {
+        mode: "GITHUB",
+        teks: "johanesPao",
+        ...warnaNormalProps
+      },
+      {
+        mode: "DOCKER",
+        teks: "j0hanespao",
+        ...warnaNormalProps
+      }
+    ]
+  },
 };
