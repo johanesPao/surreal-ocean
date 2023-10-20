0f1378a7e8570395a1557d3e13ed0f0e9fe771f7
Johanes Indra Pradana Pao <johanes.pao@gmail.com>
2023-10-20 16:49:26 +0700

    Fix warna LayarMemuat dan temporer ENV dalam Dockerfile

--- ui/src/komponen/LayarMemuat.tsx
+++ ui/src/komponen/LayarMemuat.tsx
@@ -1,7 +1,7 @@
 const LayarMemuat = () => {
   return (
-    <div className="flex w-full h-screen bg-teks items-center justify-center">
-      <span className="font-mono text-sm text-primer">{`${import.meta.env.VITE_NAMA_SITUS} memuat...`}</span>
+    <div className="flex w-full h-screen bg-primer items-center justify-center">
+      <span className="font-mono text-sm text-teks">{`${import.meta.env.VITE_NAMA_SITUS} memuat...`}</span>
     </div>
   )
     ;
