a50b86475993a3eab54bd5b7623aa53fd61351f2
Johanes Indra Pradana Pao <johanes.pao@gmail.com>
2023-11-07 17:01:28 +0700

    Pengerjaan Proyek.tsx (II)

--- ui/src/komponen/Proyek.tsx
+++ ui/src/komponen/Proyek.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { tarikEventRepo, typeGuardGitHubRespon } from "../fungsi/gh_api";
 import { EResponGitHub } from "../enum";
 import { GitHubEvent } from "../types";
+import { IconAccessible, IconCheck, IconForbid } from "@tabler/icons-react";
 
 const Proyek = ({ repo }: { repo: string }) => {
     const [data, setData] = useState<GitHubEvent | string>(""); // Assuming you've defined the Event type as shown before
@@ -39,21 +40,37 @@ const Proyek = ({ repo }: { repo: string }) => {
 
     return (
         <>
-            <div className="flex bg-stone-900 text-xs p-5 rounded-lg">
+            <div className="flex bg-stone-900 text-xs p-0 rounded-2xl overflow-hidden h-auto">
                 {memuat
                     ? <span>Memuat...</span>
                     : (kesalahanFetching && (typeof data === 'string'))
                         ? <span>Terjadi kesalahan dalam fetching:<br /><span>{data}</span></span>
-                        : (typeof (data) === 'object')
-                            ? (<div>
-                                <span>{data.actor.avatar_url}</span>
-                                {data.repo.name}
-                                {data.public}
-                                {data.created_at}
-                                {data.payload.commits![0].message}
-                                {data.payload.commits![0].sha}
-                            </div>)
-                            : "Ada deh"
+                        : (typeof data === 'object')
+                        && (
+                            <div className="flex p-5">
+                                <div className="flex flex-col">
+                                    <div className="flex gap-2 items-baseline">
+                                        <p className="font-cornerstone text-lg">{data.repo.name}</p>
+                                        <p>{data.public
+                                            ? (
+                                                <div className="flex items-center gap-1 bg-green-200 rounded-xl text-slate-950 px-3 w-auto">
+                                                    <IconForbid size={12} />
+                                                    <span>
+                                                        public
+                                                    </span>
+                                                </div>
+                                            )
+                                            : (
+                                                <IconForbid />
+                                            )}
+                                        </p>
+                                    </div>
+                                    <p>Latest commit on {(new Date(`${data.created_at}`)).toLocaleString()}</p>
+                                    <p>Commit message {data.payload.commits![0].message}</p>
+                                    <p>Commit SHA {data.payload.commits![0].sha}</p>
+                                </div>
+                            </div>
+                        )
                 }
             </div>
         </>
